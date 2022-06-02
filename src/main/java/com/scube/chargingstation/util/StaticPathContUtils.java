0413430f78be87bbb3f2f4fb2372c07ff0a1d95d
sarveshpandit1813 <sarvesh.pandit1813@gmail.com>
2022-06-02 14:27:52 +0530

    Deployment URL

--- src/main/java/com/scube/chargingstation/util/StaticPathContUtils.java
+++ src/main/java/com/scube/chargingstation/util/StaticPathContUtils.java
@@ -7,14 +7,19 @@ public class StaticPathContUtils {
 	//	public static final String APP_URL_DIR = "http://3.7.55.182/chargingStationApi/api/v1/";
 	
 	// internal server		
-		public static final String APP_URL_DIR = "http://125.99.153.126:8085/chargingStationApi/api/v1/";
+//		public static final String APP_URL_DIR = "http://125.99.153.126:8085/chargingStationApi/api/v1/";
+//		public static final String SERVER_API_URL = "http://125.99.153.126:8085/API/";
+
+		// internal server		
+		public static final String APP_URL_DIR = "https://evdock.app/chargingStationApi/api/v1/";
+		public static final String SERVER_API_URL = "https://evdock.app/API/";
 		
 		public static final String SET_RECEIPT_FILE_URL_DIR = "images/getDoc/RC/";
 		
 		public static final String SET_CHARGER_TYPE_FILE_URL_DIR = "images/getImage/CT/";
 		
 		//internal server
-		public static final String SERVER_API_URL = "http://125.99.153.126:8080/API/";
+		
 		
 		//aws 
 		//public static final String SERVER_API_URL = "http://3.7.55.182/API/";
