377b57ab8cc9054be64f05a58309191418f82837
sarveshpandit1813 <sarvesh.pandit1813@gmail.com>
2022-07-07 15:26:22 +0530

    Booking Request Entity

---
+++ src/main/java/com/scube/chargingstation/entity/BookingRequestEntity.java
@@ -0,0 +1,46 @@
+package com.scube.chargingstation.entity;
+
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table (name = "booking_request")
+@Getter
+@Setter
+public class BookingRequestEntity extends BaseEntity{
+	
+	@Column (name = "booking_amount", columnDefinition = "double default 0")
+	private double bookingAmount;
+	
+	@Column (name = "booking_status")
+	private String bookingStatus;
+	
+	@OneToOne
+    @JoinColumn(name = "fk_user")
+	private UserInfoEntity userInfoEntity;
+
+	@OneToOne
+    @JoinColumn(name = "fk_charging_point")
+	private ChargingPointEntity chargingPointEntity;
+	
+	@OneToOne
+    @JoinColumn(name = "fk_connector")
+	private ChargerTypeEntity chargerTypeEntity;
+	
+	@OneToOne
+    @JoinColumn(name = "fk_charge_request")
+	private ChargingRequestEntity chargingRequestEntity;
+	
+	@Column (name = "booking_time")
+	private Instant bookingTime;
+	
+}
