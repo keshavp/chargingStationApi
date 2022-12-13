ef4b22e11f2c52f77548904021b286bb406c3927
sarveshpandit1813 <sarvesh.pandit1813@gmail.com>
2022-12-13 16:02:44 +0530

    Booking Receipt Val Changes

--- src/main/java/com/scube/chargingstation/util/StaticPathContUtils.java
+++ src/main/java/com/scube/chargingstation/util/StaticPathContUtils.java
@@ -2,18 +2,21 @@ package com.scube.chargingstation.util;
 
 public class StaticPathContUtils {
 
-	
+	// Sarvesh Local
+//		public static final String APP_URL_DIR = "http://192.168.0.210:8081/chargingStationApi/api/v1/";
 	
 	// internal server		
 	//	public static final String APP_URL_DIR = "http://125.99.153.126:8085/chargingStationApi/api/v1/";
 	//	public static final String SERVER_API_URL = "http://125.99.153.126:8080/API/";
 
 		// aws server		
-		public static final String APP_URL_DIR = "https://evdock.app/chargingStationApi/api/v1/";
+//		public static final String APP_URL_DIR = "https://evdock.app/chargingStationApi/api/v1/";
 		public static final String SERVER_API_URL = "http://evdock.app:8082/API/";
 		
 		public static final String SET_RECEIPT_FILE_URL_DIR = "images/getDoc/RC/";
+		public static final String SET_BOOKING_RECEIPT_FILE_URL_DIR = "images/getDoc/BRC/";
 		public static final String SET_CHARGER_TYPE_FILE_URL_DIR = "images/getImage/CT/";
+		public static final String SET_CAR_MODEL_TYPE_FILE_URL_DIR = "images/getImage/CMT/";
 		   
 		//AWS
 		public static final String CCAVENUE_CANCELURL = "https://evdock.app/chargingStation/cancelUrl";
